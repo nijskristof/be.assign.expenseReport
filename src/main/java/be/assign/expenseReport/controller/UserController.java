caf01eff212e36231f7d43ecfd8ca50c915d5270
nijskristof <nijskristof@hotmail.com>
2016-07-14 08:47:43 +0200

    1. Changes in the infrastructure. 2. Get request for a user and a list of users

---
+++ src/main/java/be/assign/expenseReport/controller/UserController.java
@@ -0,0 +1,40 @@
+package be.assign.expenseReport.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.assign.expenseReport.dao.UserDao;
+import be.assign.expenseReport.model.User;
+
+public class UserController  {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@RequestMapping(value="/")
+	public ModelAndView listUser(ModelAndView model) throws IOException{
+	    List<User> listUser = userDao.list();
+	    model.addObject("listUser", listUser);
+	    model.setViewName("user");
+	 
+	    return model;
+	}
+	
+	@RequestMapping(value = "/selectContact", method = RequestMethod.GET)
+	public ModelAndView editContact(HttpServletRequest request) {
+	    int userId = Integer.parseInt(request.getParameter("id"));
+	    User user = userDao.getUser(userId);
+	    ModelAndView model = new ModelAndView("UserForm");
+	    model.addObject("user", user);
+	 
+	    return model;
+	}
+	
+}
